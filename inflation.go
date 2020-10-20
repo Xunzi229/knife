091c913f938c4202fbece61a26c07b200acb7f59
xu.zhi <xuzhi@boohee.com>
2020-10-20 15:38:19 +0800

    增加忽略文件

--- inflation.go
+++ inflation.go
@@ -1,8 +1,8 @@
 package knife
 
 import (
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 )
 
 type Inflection struct {
@@ -12,15 +12,14 @@ type Inflection struct {
 }
 
 var (
-	Plurals = make([]Inflection, 35)
-	Singulars = make([]Inflection, 41)
+	Plurals      = make([]Inflection, 35)
+	Singulars    = make([]Inflection, 41)
 	Uncountables = make([]Inflection, 11)
-	MaxSeg = 10
 )
 
 func init() {
 	var reg *regexp.Regexp
- 
+
 	reg = regexp.MustCompile(`(?i)(h)ouse_corpuses$`)
 	Plurals[0] = Inflection{Reg: reg, Replace: `${1}ouse_corpuses`}
 	reg = regexp.MustCompile(`(?i)(h)ouse_corpus$`)
@@ -91,120 +90,119 @@ func init() {
 	Plurals[33] = Inflection{Reg: reg, Replace: "s", Sites: []int{}}
 	reg = regexp.MustCompile(`$`)
 	Plurals[34] = Inflection{Reg: reg, Replace: "s", Sites: []int{}}
- 
+
 	reg = regexp.MustCompile(`(?i)(h)ouse_corpuses$`)
-    Singulars[0] = Inflection{Reg: reg, Replace: "${1}ouse_corpus"}
-    reg = regexp.MustCompile(`(?i)(h)ouse_corpus$`)
-    Singulars[1] = Inflection{Reg: reg, Replace: "${1}ouse_corpus"}
-    reg = regexp.MustCompile(`(?i)(z)ombies$`)
-    Singulars[2] = Inflection{Reg: reg, Replace: "${1}ombie"}
-    reg = regexp.MustCompile(`(?i)(z)ombie$`)
-    Singulars[3] = Inflection{Reg: reg, Replace: "${1}ombie"}
-    reg = regexp.MustCompile(`(?i)(m)oves$`)
-    Singulars[4] = Inflection{Reg: reg, Replace: "${1}ove"}
-    reg = regexp.MustCompile(`(?i)(m)ove$`)
-    Singulars[5] = Inflection{Reg: reg, Replace: "${1}ove"}
-    reg = regexp.MustCompile(`(?i)(s)exes$`)
-    Singulars[6] = Inflection{Reg: reg, Replace: "${1}ex"}
-    reg = regexp.MustCompile(`(?i)(s)ex$`)
-    Singulars[7] = Inflection{Reg: reg, Replace: "${1}ex"}
-    reg = regexp.MustCompile(`(?i)(c)hildren$`)
-    Singulars[8] = Inflection{Reg: reg, Replace: "${1}hild"}
-    reg = regexp.MustCompile(`(?i)(c)hild$`)
-    Singulars[9] = Inflection{Reg: reg, Replace: "${1}hild"}
-    reg = regexp.MustCompile(`(?i)(m)en$`)
-    Singulars[10] = Inflection{Reg: reg, Replace: "${1}an"}
-    reg = regexp.MustCompile(`(?i)(m)an$`)
-    Singulars[11] = Inflection{Reg: reg, Replace: "${1}an"}
-    reg = regexp.MustCompile(`(?i)(p)eople$`)
-    Singulars[12] = Inflection{Reg: reg, Replace: "${1}erson"}
-    reg = regexp.MustCompile(`(?i)(p)erson$`)
-    Singulars[13] = Inflection{Reg: reg, Replace: "${1}erson"}
-    reg = regexp.MustCompile(`(?i)(database)s$`)
-    Singulars[14] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(quiz)zes$`)
-    Singulars[15] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(matr)ices$`)
-    Singulars[16] = Inflection{Reg: reg, Replace: "${1}ix"}
-    reg = regexp.MustCompile(`(?i)(vert|ind)ices$`)
-    Singulars[17] = Inflection{Reg: reg, Replace: "${1}ex"}
-    reg = regexp.MustCompile(`(?i)^(ox)en`)
-    Singulars[18] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(alias|status)(es)?$`)
-    Singulars[19] = Inflection{Reg: reg, Replace: ""}
-    reg = regexp.MustCompile(`(?i)(octop|vir)(us|i)$`)
-    Singulars[20] = Inflection{Reg: reg, Replace: "${1}us"}
-    reg = regexp.MustCompile(`(?i)^(a)x[ie]s$`)
-    Singulars[21] = Inflection{Reg: reg, Replace: "${1}xis"}
-    reg = regexp.MustCompile(`(?i)(cris|test)(is|es)$`)
-    Singulars[22] = Inflection{Reg: reg, Replace: "${1}is"}
-    reg = regexp.MustCompile(`(?i)(shoe)s$`)
-    Singulars[23] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(o)es$`)
-    Singulars[24] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(bus)(es)?$`)
-    Singulars[25] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)^(m|l)ice$`)
-    Singulars[26] = Inflection{Reg: reg, Replace: "${1}ouse"}
-    reg = regexp.MustCompile(`(?i)(x|ch|ss|sh)es$`)
-    Singulars[27] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(m)ovies$`)
-    Singulars[28] = Inflection{Reg: reg, Replace: "${1}ovie"}
-    reg = regexp.MustCompile(`(?i)(s)eries$`)
-    Singulars[29] = Inflection{Reg: reg, Replace: "${1}eries"}
-    reg = regexp.MustCompile(`(?i)([^aeiouy]|qu)ies$`)
-    Singulars[30] = Inflection{Reg: reg, Replace: "${1}y"}
-    reg = regexp.MustCompile(`(?i)([lr])ves$`)
-    Singulars[31] = Inflection{Reg: reg, Replace: "${1}f"}
-    reg = regexp.MustCompile(`(?i)(tive)s$`)
-    Singulars[32] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)(hive)s$`)
-    Singulars[33] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)([^f])ves$`)
-    Singulars[34] = Inflection{Reg: reg, Replace: "${1}fe"}
-    reg = regexp.MustCompile(`(?i)(^analy)(sis|ses)$`)
-    Singulars[35] = Inflection{Reg: reg, Replace: "${1}sis"}
-    reg = regexp.MustCompile(`(?i)((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$`)
-    Singulars[36] = Inflection{Reg: reg, Replace: "${1}sis"}
-    reg = regexp.MustCompile(`(?i)([ti])a$`)
-    Singulars[37] = Inflection{Reg: reg, Replace: "${1}um"}
-    reg = regexp.MustCompile(`(?i)(n)ews$`)
-    Singulars[38] = Inflection{Reg: reg, Replace: "${1}ews"}
-    reg = regexp.MustCompile(`(?i)(ss)$`)
-    Singulars[39] = Inflection{Reg: reg, Replace: "${1}"}
-    reg = regexp.MustCompile(`(?i)s$`)
-    Singulars[40] = Inflection{Reg: reg, Replace: "", Sites: []int{}}
-	
-    
-    reg = regexp.MustCompile(`equipment`)
-    Uncountables[0] = Inflection{Reg: reg, Replace: "equipment"}
-    reg = regexp.MustCompile(`information`)
-    Uncountables[1] = Inflection{Reg: reg, Replace: "information"}
-    reg = regexp.MustCompile(`rice`)
-    Uncountables[2] = Inflection{Reg: reg, Replace: "rice"}
-    reg = regexp.MustCompile(`money`)
-    Uncountables[3] = Inflection{Reg: reg, Replace: "money"}
-    reg = regexp.MustCompile(`species`)
-    Uncountables[4] = Inflection{Reg: reg, Replace: "species"}
-    reg = regexp.MustCompile(`series`)
-    Uncountables[5] = Inflection{Reg: reg, Replace: "series"}
-    reg = regexp.MustCompile(`fish`)
-    Uncountables[6] = Inflection{Reg: reg, Replace: "fish"}
-    reg = regexp.MustCompile(`sheep`)
-    Uncountables[7] = Inflection{Reg: reg, Replace: "sheep"}
-    reg = regexp.MustCompile(`jeans`)
-    Uncountables[9] = Inflection{Reg: reg, Replace: "jeans"}
-    reg = regexp.MustCompile(`police`)
-    Uncountables[10] = Inflection{Reg: reg, Replace: "police"}
+	Singulars[0] = Inflection{Reg: reg, Replace: "${1}ouse_corpus"}
+	reg = regexp.MustCompile(`(?i)(h)ouse_corpus$`)
+	Singulars[1] = Inflection{Reg: reg, Replace: "${1}ouse_corpus"}
+	reg = regexp.MustCompile(`(?i)(z)ombies$`)
+	Singulars[2] = Inflection{Reg: reg, Replace: "${1}ombie"}
+	reg = regexp.MustCompile(`(?i)(z)ombie$`)
+	Singulars[3] = Inflection{Reg: reg, Replace: "${1}ombie"}
+	reg = regexp.MustCompile(`(?i)(m)oves$`)
+	Singulars[4] = Inflection{Reg: reg, Replace: "${1}ove"}
+	reg = regexp.MustCompile(`(?i)(m)ove$`)
+	Singulars[5] = Inflection{Reg: reg, Replace: "${1}ove"}
+	reg = regexp.MustCompile(`(?i)(s)exes$`)
+	Singulars[6] = Inflection{Reg: reg, Replace: "${1}ex"}
+	reg = regexp.MustCompile(`(?i)(s)ex$`)
+	Singulars[7] = Inflection{Reg: reg, Replace: "${1}ex"}
+	reg = regexp.MustCompile(`(?i)(c)hildren$`)
+	Singulars[8] = Inflection{Reg: reg, Replace: "${1}hild"}
+	reg = regexp.MustCompile(`(?i)(c)hild$`)
+	Singulars[9] = Inflection{Reg: reg, Replace: "${1}hild"}
+	reg = regexp.MustCompile(`(?i)(m)en$`)
+	Singulars[10] = Inflection{Reg: reg, Replace: "${1}an"}
+	reg = regexp.MustCompile(`(?i)(m)an$`)
+	Singulars[11] = Inflection{Reg: reg, Replace: "${1}an"}
+	reg = regexp.MustCompile(`(?i)(p)eople$`)
+	Singulars[12] = Inflection{Reg: reg, Replace: "${1}erson"}
+	reg = regexp.MustCompile(`(?i)(p)erson$`)
+	Singulars[13] = Inflection{Reg: reg, Replace: "${1}erson"}
+	reg = regexp.MustCompile(`(?i)(database)s$`)
+	Singulars[14] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(quiz)zes$`)
+	Singulars[15] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(matr)ices$`)
+	Singulars[16] = Inflection{Reg: reg, Replace: "${1}ix"}
+	reg = regexp.MustCompile(`(?i)(vert|ind)ices$`)
+	Singulars[17] = Inflection{Reg: reg, Replace: "${1}ex"}
+	reg = regexp.MustCompile(`(?i)^(ox)en`)
+	Singulars[18] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(alias|status)(es)?$`)
+	Singulars[19] = Inflection{Reg: reg, Replace: ""}
+	reg = regexp.MustCompile(`(?i)(octop|vir)(us|i)$`)
+	Singulars[20] = Inflection{Reg: reg, Replace: "${1}us"}
+	reg = regexp.MustCompile(`(?i)^(a)x[ie]s$`)
+	Singulars[21] = Inflection{Reg: reg, Replace: "${1}xis"}
+	reg = regexp.MustCompile(`(?i)(cris|test)(is|es)$`)
+	Singulars[22] = Inflection{Reg: reg, Replace: "${1}is"}
+	reg = regexp.MustCompile(`(?i)(shoe)s$`)
+	Singulars[23] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(o)es$`)
+	Singulars[24] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(bus)(es)?$`)
+	Singulars[25] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)^(m|l)ice$`)
+	Singulars[26] = Inflection{Reg: reg, Replace: "${1}ouse"}
+	reg = regexp.MustCompile(`(?i)(x|ch|ss|sh)es$`)
+	Singulars[27] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(m)ovies$`)
+	Singulars[28] = Inflection{Reg: reg, Replace: "${1}ovie"}
+	reg = regexp.MustCompile(`(?i)(s)eries$`)
+	Singulars[29] = Inflection{Reg: reg, Replace: "${1}eries"}
+	reg = regexp.MustCompile(`(?i)([^aeiouy]|qu)ies$`)
+	Singulars[30] = Inflection{Reg: reg, Replace: "${1}y"}
+	reg = regexp.MustCompile(`(?i)([lr])ves$`)
+	Singulars[31] = Inflection{Reg: reg, Replace: "${1}f"}
+	reg = regexp.MustCompile(`(?i)(tive)s$`)
+	Singulars[32] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)(hive)s$`)
+	Singulars[33] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)([^f])ves$`)
+	Singulars[34] = Inflection{Reg: reg, Replace: "${1}fe"}
+	reg = regexp.MustCompile(`(?i)(^analy)(sis|ses)$`)
+	Singulars[35] = Inflection{Reg: reg, Replace: "${1}sis"}
+	reg = regexp.MustCompile(`(?i)((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$`)
+	Singulars[36] = Inflection{Reg: reg, Replace: "${1}sis"}
+	reg = regexp.MustCompile(`(?i)([ti])a$`)
+	Singulars[37] = Inflection{Reg: reg, Replace: "${1}um"}
+	reg = regexp.MustCompile(`(?i)(n)ews$`)
+	Singulars[38] = Inflection{Reg: reg, Replace: "${1}ews"}
+	reg = regexp.MustCompile(`(?i)(ss)$`)
+	Singulars[39] = Inflection{Reg: reg, Replace: "${1}"}
+	reg = regexp.MustCompile(`(?i)s$`)
+	Singulars[40] = Inflection{Reg: reg, Replace: "", Sites: []int{}}
+
+	reg = regexp.MustCompile(`equipment`)
+	Uncountables[0] = Inflection{Reg: reg, Replace: "equipment"}
+	reg = regexp.MustCompile(`information`)
+	Uncountables[1] = Inflection{Reg: reg, Replace: "information"}
+	reg = regexp.MustCompile(`rice`)
+	Uncountables[2] = Inflection{Reg: reg, Replace: "rice"}
+	reg = regexp.MustCompile(`money`)
+	Uncountables[3] = Inflection{Reg: reg, Replace: "money"}
+	reg = regexp.MustCompile(`species`)
+	Uncountables[4] = Inflection{Reg: reg, Replace: "species"}
+	reg = regexp.MustCompile(`series`)
+	Uncountables[5] = Inflection{Reg: reg, Replace: "series"}
+	reg = regexp.MustCompile(`fish`)
+	Uncountables[6] = Inflection{Reg: reg, Replace: "fish"}
+	reg = regexp.MustCompile(`sheep`)
+	Uncountables[7] = Inflection{Reg: reg, Replace: "sheep"}
+	reg = regexp.MustCompile(`jeans`)
+	Uncountables[9] = Inflection{Reg: reg, Replace: "jeans"}
+	reg = regexp.MustCompile(`police`)
+	Uncountables[10] = Inflection{Reg: reg, Replace: "police"}
 }
 
 func uncountable(str string) bool {
-    for i := 0; i < len(Uncountables); i++ {
-        if Uncountables[i].Reg.Match([]byte(str)) {
-            return true
-        }
-    }
-    return false
+	for i := 0; i < len(Uncountables); i++ {
+		if Uncountables[i].Reg.Match([]byte(str)) {
+			return true
+		}
+	}
+	return false
 }
 
 // Makes an underscored, lowercase form from the expression in the string.
@@ -219,34 +217,34 @@ func uncountable(str string) bool {
 //
 //  Camelize(underscore('SSLError'))  # => "SslError"
 func underscore(word string) string {
-    reg := regexp.MustCompile(`[A-Z-]|::`)
-    if !reg.Match([]byte(word)) {
-        return word
-    }
-    reg = regexp.MustCompile(`::`)
-    word = string(reg.ReplaceAll([]byte(word), []byte("/")))
-    
-    //word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
-    
-    reg = regexp.MustCompile(`([A-Z\d]+)([A-Z][a-z])`)
-    word = reg.ReplaceAllString(word, "${1}_${2}")
-    
-    reg = regexp.MustCompile(`([a-z\d])([A-Z])`)
-    word = reg.ReplaceAllString(word, "${1}_${2}")
-    
-    reg = regexp.MustCompile(`-`)
-    word = string(reg.ReplaceAll([]byte(word), []byte("_")))
-    
-    word = strings.ToLower(word)
-    return word
+	reg := regexp.MustCompile(`[A-Z-]|::`)
+	if !reg.Match([]byte(word)) {
+		return word
+	}
+	reg = regexp.MustCompile(`::`)
+	word = string(reg.ReplaceAll([]byte(word), []byte("/")))
+
+	//word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
+
+	reg = regexp.MustCompile(`([A-Z\d]+)([A-Z][a-z])`)
+	word = reg.ReplaceAllString(word, "${1}_${2}")
+
+	reg = regexp.MustCompile(`([a-z\d])([A-Z])`)
+	word = reg.ReplaceAllString(word, "${1}_${2}")
+
+	reg = regexp.MustCompile(`-`)
+	word = string(reg.ReplaceAll([]byte(word), []byte("_")))
+
+	word = strings.ToLower(word)
+	return word
 }
 
 func applyInflections(word string, rules []Inflection) string {
-    if len(word) == 0 && uncountable(word) {
-        return word
-    }
-    
-    for i := 0; i < len(rules); i++ {
+	if len(word) == 0 && uncountable(word) {
+		return word
+	}
+
+	for i := 0; i < len(rules); i++ {
 		sp := rules[i]
 		reg := sp.Reg
 		if sb := reg.FindSubmatch([]byte(word)); len(sb) > 0 {
@@ -254,5 +252,5 @@ func applyInflections(word string, rules []Inflection) string {
 			return word
 		}
 	}
-    return word
+	return word
 }
